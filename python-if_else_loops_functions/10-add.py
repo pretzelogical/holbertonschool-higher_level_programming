607964607d83b08df6e2f85ec769f65719b41bdb
pretzelogical <yingyangyo266@gmail.com>
2023-05-09 20:33:02 -0500

    add 10-12

---
+++ python-if_else_loops_functions/10-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author: pretzelogic
+# def add(a, b): adds two integers and returns the result
+def add(a, b):
+    return a + b
---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# Author: pretzelogic
+# def fizzbuzz(): prints the numbers 1 to 100 
+# if a number is a multiple of 3 print Fizz
+# if a number is a multiple of 5 print Buzz
+# if number is a multiple of both 3 & 5 print FizzBuzz
+def fizzbuzz():
+    for i in range(1,101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
