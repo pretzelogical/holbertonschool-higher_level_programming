663deedbda48b7d617c5ff8d5b880d739601bbee
pretzelogical <yingyangyo266@gmail.com>
2023-05-09 21:20:48 -0500

    fix 12 pep8 formatting

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 # Author: pretzelogic
-# def fizzbuzz(): prints the numbers 1 to 100 
+# def fizzbuzz(): prints the numbers 1 to 100
 # if a number is a multiple of 3 print Fizz
 # if a number is a multiple of 5 print Buzz
 # if number is a multiple of both 3 & 5 print FizzBuzz
 
 
 def fizzbuzz():
-    for i in range(1,101):
+    for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
         elif i % 3 == 0:
