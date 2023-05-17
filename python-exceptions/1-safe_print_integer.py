e4228aadb0c68ead5dd90b627848f3fec79e2260
pretzelogical <yingyangyo266@gmail.com>
2023-05-16 23:03:48 -0500

    fix 1-sa_pr_int format

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -5,7 +5,7 @@ def safe_print_integer(value):
     """Prints an integer
     Returns True if succesful, else false"""
     try:
-        print(":d".format(value))
+        print("{:d}".format(value))
         return True
     except (TypeError, ValueError):
         return False
