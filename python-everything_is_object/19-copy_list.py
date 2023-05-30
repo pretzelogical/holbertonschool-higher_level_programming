3cdcf73773bf4f65ddc250df5a8cbf52b0488491
pretzelogical <yingyangyo266@gmail.com>
2023-05-30 11:59:26 -0500

    make 19-copy_list 3 lines

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
-"""Function that copies a list"""
-
-
 def copy_list(a_list):
-    """Returns a copy of a list
-
-    Args:
-        a_list (list): list to copy
-
-    Returns:
-        list: copied list
-    """
     return a_list[:]
