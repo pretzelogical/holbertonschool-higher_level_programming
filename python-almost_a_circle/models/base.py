e42e1c69104f2c156f4b6607634dbd8d2e21e20f
pretzelogical <yingyangyo266@gmail.com>
2023-06-01 11:41:36 -0500

    add python-almost_a_circle and start task 1, need to read more on packages/modules to get tests properly set up

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Base class for all other classes in the project"""
+
+
+class Base:
+    """Base class for all other classes in the project"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initializes the Base class
+
+        Args:
+            id (int, optional): id to be assigned to keep track
+            of object if id is None increments __nb_objects by one
+            and assigns it to the object id. Defaults to None.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
