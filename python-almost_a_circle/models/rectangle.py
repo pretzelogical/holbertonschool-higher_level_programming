eecbe61455e9ef70db547431a8f2e9819a3e9ca5
pretzelogical <yingyangyo266@gmail.com>
2023-06-02 09:08:17 -0500

    Add more documentation to rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -4,10 +4,22 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """Class that represents a Rectangle"""
+    """Class that represents a Rectangle.
+
+    Args:
+        Base (Class): Helper class to keep track of instances
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize Rectangle"""
+        """Initializes a Rectangle instance.
+
+        Args:
+            width (int): Width of the rectangle
+            height (int): Height of the rectangle
+            x (int): X coordinate of the rectangle
+            y (int): Y coordinate of the rectangle
+            id (int): Id of the rectangle
+        """
         super().__init__(id)
         self.__width = width
         self.__height = height
@@ -17,40 +29,40 @@ class Rectangle(Base):
     # getters and setters
     @property
     def x(self):
-        """Get x"""
+        """Get the private x attribute"""
         return self.__x
 
     @x.setter
     def x(self, value):
-        """Set x"""
+        """Set the private x attribute"""
         self.__x = value
 
     @property
     def y(self):
-        """Get y"""
+        """Get the private y attribute"""
         return self.__y
 
     @y.setter
     def y(self, value):
-        """Set y"""
+        """Set the private y attribute"""
         self.__y = value
 
     @property
     def width(self):
-        """Get width"""
+        """Get the private width attribute"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Set width"""
+        """Set the private width attribute"""
         self.__width = value
 
     @property
     def height(self):
-        """Get height"""
+        """Get the private height attribute"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Set height"""
+        """Set the private height attribute"""
         self.__height = value
