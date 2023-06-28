55f63cc9255f7746e2131ab352554e1966d54e2e
pretzelogical <yingyangyo266@gmail.com>
2023-06-28 15:05:14 -0500

    change exists to where

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,4 +1,5 @@
 -- lists all records of second_table in hbtn_0c_0
-SELECT (score, name) FROM second_table
-WHERE EXISTS (SELECT name FROM second_table)
+SELECT (score, name)
+FROM second_table
+WHERE name != ""
 ORDER BY score DESC;
