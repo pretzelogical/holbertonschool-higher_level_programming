dd60afbd21a1b39f5d80644693ece84717959798
pretzelogical <yingyangyo266@gmail.com>
2023-07-03 08:22:20 -0500

    make 9 use orm features

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -13,10 +13,10 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).order_by(State.id).all()
+    states = (session.query(State).filter(State.name.like('%a%'))
+              .order_by(State.id).all())
 
     for state in states:
-        if 'a' in state.name:
-            print(f"{state.id}: {state.name}")
+        print(f"{state.id}: {state.name}")
 
     session.close()
