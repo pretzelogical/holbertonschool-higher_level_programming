d316120c51c7369f7bf1fdf3e099ab186162c843
pretzelogical <yingyangyo266@gmail.com>
2023-07-03 09:58:33 -0500

    14 v1

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" Deletes state objects with a name containing a """
+""" Prints city objects from hbtn_0e_14_usa """
 import sys
 from model_state import Base, State
+from model_city import City  # type: ignore
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -11,12 +12,12 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = (session.query(State)
-              .filter(State.name.like('%a%'))
-              .all())
+    query = (session.query(State, City)
+             .filter(State.id == City.state_id)
+             .order_by(City.id)
+             .all())
 
-    for state in states:
-        session.delete(state)
-    session.commit()
+    for state, city in query:
+        print(f"{state.name}: {city.id} {city.name}")
 
     session.close()
