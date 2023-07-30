1de453afe771436111f95fcea0a2b1d098b71a96
pretzelogical <yingyangyo266@gmail.com>
2023-07-30 12:28:35 -0500

    fix 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-console.log(process.argv);
-console.log(process.argv.length);
 let argMessage;
-if (process.argv.length > 2) {
+if (process.argv.length === 3) {
   argMessage = 'Argument found';
+} else if (process.argv.length > 3) {
+  argMessage = 'Arguments found';
 } else {
   argMessage = 'No argument';
 }
