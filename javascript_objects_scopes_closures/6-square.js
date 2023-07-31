f0d7a50e92df5721f5292ed3bf7021d5e99e7d3d
pretzelogical <yingyangyo266@gmail.com>
2023-07-31 11:32:32 -0500

    fix 6 style

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -7,10 +7,10 @@ module.exports = class Square extends Rectangle {
 
   charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
-        for (let j = 0; j < this.width; j++) {
-          process.stdout.write(c);
-        }
-        console.log();
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write(c);
       }
+      console.log();
+    }
   }
 };
