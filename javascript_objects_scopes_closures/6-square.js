14ea69a688a5d068cb0b509340803d516033aa64
pretzelogical <yingyangyo266@gmail.com>
2023-07-31 11:34:58 -0500

    make class inherit square from task 5

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+module.exports = class Square extends require('./5-square') {
   charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
