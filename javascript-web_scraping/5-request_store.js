7866481b2f628e8528fe612f56260f208722a222
pretzelogical <yingyangyo266@gmail.com>
2023-08-04 14:15:19 -0500

    Will now output blank file on blank body

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -6,11 +6,9 @@ request(process.argv[2], (error, response, body) => {
   if (error) {
     console.error(error);
   }
-  if (body) {
-    fs.writeFile(process.argv[3], body, err => {
-      if (err) {
-        console.error(err);
-      }
-    });
-  }
+  fs.writeFile(process.argv[3], body, 'utf8', err => {
+    if (err) {
+      console.error(err);
+    }
+  });
 });
